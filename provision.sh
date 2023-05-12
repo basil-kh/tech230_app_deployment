7406eed466157988bc4518462d3568a3c4052df9
Basil Kheireddine <bkheireddine0@gmail.com>
2023-05-12 11:44:17 +0100

    added extra automation steps for python and nodejs

--- provision.sh
+++ provision.sh
@@ -5,3 +5,11 @@ sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 sudo systemctl start nginx
 sudo systemctl status nginx
+sudo apt-get install python-software-properties -y
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+sudo npm install pm2 -g
+cd /home/vagrant/app
+npm install
+npm start
+
